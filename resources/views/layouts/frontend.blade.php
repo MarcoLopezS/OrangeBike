5cb7a47472bcaae3f3730d0b0ec36464a81bac77
Marco Lopez <mlopez@marostdevelopers.com>
2016-02-09 03:41:48 -0500

    enlace a widgets y contenido footer de frontend

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -3,7 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Orange Bike Perú</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>
+		@section('title')
+			Orange Bike Perú
+		@show
+	</title>
 	
 	{{-- Bootstrap y FontAwesome--}}
 	{!! HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css') !!}
@@ -25,6 +30,7 @@
 	{!! HTML::style('assets/revslider/css/settings.css') !!}
 
     {{-- Estilos --}}
+    {!! HTML::style('css/normalize.css') !!}
     {!! HTML::style('css/estilos.css') !!}
 
 	@yield('contenido_header')
@@ -68,10 +74,11 @@
 		            <div class="container">
 		                <div class="row">
 		                    <!--  Brand and toggle get grouped for better mobile display -->
-		                    <div class="col-md-3 col-sm-3 col-xs-12">
+		                    <div class="col-lg-3 col-md-2 col-sm-3 col-xs-12">
 			                    <div class="navbar-header">
-			                        <a class="navbar-brand wow bounceIn logo col-xs-8 animated" data-wow-duration="2s" data-wow-delay="1s" href="index.html" style="visibility: visible; animation-duration: 2s; animation-delay: 1s; animation-name: bounceIn;">
-			                            <img height="40" src="imagenes/logo.jpg" alt="Orange Bike Perú" class="img-responsive"> 
+			                        <a class="navbar-brand wow bounceIn logo col-xs-8 animated" data-wow-duration="2s" data-wow-delay="1s" href="/" 
+			                        style="visibility: visible; animation-duration: 2s; animation-delay: 1s; animation-name: bounceIn;">
+			                            <img height="40" src="/imagenes/logo.jpg" alt="Orange Bike Perú" class="img-responsive"> 
 			                        </a>
 			                        <div class="col-xs-4">
 				                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fixed-collapse-navbar">
@@ -84,16 +91,15 @@
 			                    </div>
 		                    </div>
 		                    <!--  Collect the nav links, forms, and other content for toggling --> 
-		                    <div class="col-md-9 col-sm-9 col-xs-12">
+		                    <div class="col-lg-9 col-md-10 col-sm-9 col-xs-12">
 			                    <div class="collapse navbar-collapse nav-collapse" id="fixed-collapse-navbar">
 			                        <ul class="nav navbar-nav navbar-right cl-effect-1">
-			                            <li class="active"><a href="#main-slider" class="scroll">Inicio</a> </li>
-			                            <li><a href="#about" class="scroll">Integrantes</a> </li>
-			                            <li><a href="#trails" class="scroll">Noticias</a> </li>
-			                            <li><a href="#trails" class="scroll">Fotos</a> </li>
-			                            <li><a href="#trails" class="scroll">Videos</a> </li>
-			                            <li><a href="#blogs" class="scroll">Bicicentro</a></li>                            
-			                            <li><a href="#contact" class="scroll">Contacto</a></li>
+			                            <li {!! (Request::is('/') ? 'class="active"' : '') !!} ><a href="/">Inicio</a></li>
+										<li {!! (Request::is('blog*') ? 'class="active"' : '') !!} ><a href="/blog">Blog</a></li>
+										<li {!! (Request::is('fotos*') ? 'class="active"' : '') !!} ><a href="/fotos">Fotos</a></li>
+										<li {!! (Request::is('videos*') ? 'class="active"' : '') !!} ><a href="/videos">Videos</a></li>
+										<li {!! (Request::is('bicicentro*') ? 'class="active"' : '') !!} ><a href="/bicicentro">Bicicentro</a></li>
+										<li {!! (Request::is('contacto*') ? 'class="active"' : '') !!} ><a href="/contacto">Contacto</a></li>
 			                        </ul>
 			                    </div>
 		                    </div>
@@ -103,40 +109,11 @@
 		    </div>
 		</div>
 
-		<div class="tp-banner-container">
-			<div class="tp-banner" >
-				<ul>
-					<li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-saveperformance="on"  data-title="Slide">
-						<img src="imagenes/peloton.jpg"  alt="slidebg1" data-lazyload="imagenes/peloton.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-					</li>
-
-					<li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-saveperformance="on"  data-title="Slide">
-						<img src="imagenes/llegada.jpg"  alt="slidebg1" data-lazyload="imagenes/llegada.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-					</li>
-
-					<li data-transition="fade" data-slotamount="7" data-masterspeed="500" data-saveperformance="on"  data-title="Slide">
-						<img src="imagenes/sprinter.jpg"  alt="slidebg1" data-lazyload="imagenes/sprinter.jpg" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
-					</li>
-
-				</ul>
-
-				<div class="tp-bannertimer"></div>
-
-			</div>
-
-		</div>
+		@include('frontend.widgets.slider')
 		
 	</header>
 
-	<section>
-
-		<div class="container">
-
-			@yield('contenido_body')
-
-		</div>
-
-	</section>
+	@yield('contenido_body')
 	
 	<footer>
 
@@ -155,70 +132,6 @@
 	{{-- jQuery y Bootstrap--}}
 	{!! HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js') !!}
 	{!! HTML::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js') !!}
-
-	{{-- Revolution Slider --}}
-	{!! HTML::script('assets/revslider/js/jquery.themepunch.tools.min.js') !!}
-	{!! HTML::script('assets/revslider/js/jquery.themepunch.revolution.min.js') !!}
-	<script>
-		jQuery(document).ready(function(){
-			jQuery('.tp-banner').show().revolution({
-				dottedOverlay:"none",
-				delay:16000,
-				startwidth:1170,
-				startheight:500,
-				hideThumbs:200,
-				thumbWidth:100,
-				thumbHeight:50,
-				thumbAmount:5,
-				navigationType:"bullet",
-				navigationArrows:"solo",
-				navigationStyle:"round",
-				touchenabled:"on",
-				onHoverStop:"on",
-				swipe_velocity: 0.7,
-				swipe_min_touches: 1,
-				swipe_max_touches: 1,
-				drag_block_vertical: false,
-				parallax:"mouse",
-				parallaxBgFreeze:"on",
-				parallaxLevels:[7,4,3,2,5,4,3,2,1,0],
-				keyboardNavigation:"off",
-				navigationHAlign:"center",
-				navigationVAlign:"bottom",
-				navigationHOffset:0,
-				navigationVOffset:20,
-				soloArrowLeftHalign:"left",
-				soloArrowLeftValign:"center",
-				soloArrowLeftHOffset:20,
-				soloArrowLeftVOffset:0,
-				soloArrowRightHalign:"right",
-				soloArrowRightValign:"center",
-				soloArrowRightHOffset:20,
-				soloArrowRightVOffset:0,
-				shadow:0,
-				fullWidth:"on",
-				fullScreen:"off",
-				spinner:"spinner4",
-				stopLoop:"off",
-				stopAfterLoops:-1,
-				stopAtSlide:-1,
-				shuffle:"off",
-				autoHeight:"off",
-				forceFullWidth:"off",
-				hideThumbsOnMobile:"off",
-				hideNavDelayOnMobile:1500,
-				hideBulletsOnMobile:"off",
-				hideArrowsOnMobile:"off",
-				hideThumbsUnderResolution:0,
-				hideSliderAtLimit:0,
-				hideCaptionAtLimit:0,
-				hideAllCaptionAtLilmit:0,
-				startWithSlide:0,
-				videoJsPath:"rs-plugin/videojs/",
-				fullScreenOffsetContainer: ""
-			});
-		});
-	</script>
 
 	@yield('contenido_footer')
 
